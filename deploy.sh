d0101d7c6980a5ea0ea6ad90706375cb35d8db45
Greg Walker <mgwalker@gmail.com>
2017-02-21 21:57:06 -0600

    Should definitely go into detached mode... but circle still didn't kick off a deploy :(

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 KEY=$(ls ~/.ssh | egrep '^id_[0-9a-f]{32}$')
 docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=~/.ssh/$KEY deploy-target
 eval $(docker-machine env deploy-target)
-docker-compose up --build
+docker-compose up --build -d
