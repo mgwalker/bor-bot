1e03f9a8dadfd369fef12de68f1beb2913bb0d00
Greg Walker <mgwalker@gmail.com>
2017-02-21 22:31:29 -0600

    Some commentary so I know what this is all doing in the future

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# Download docker-machine
 curl -L https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-`uname -s`-`uname -m` > ./docker-machine
 chmod +x ./docker-machine
 
 # Find our deployment key
 KEY=$(ls ~/.ssh | egrep '^id_[0-9a-f]{32}$')
+
+# Add remote docker host
 ./docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=/home/ubuntu/.ssh/$KEY deploy-target
 eval $(./docker-machine env deploy-target)
+
+# Go!
 docker-compose -f docker-compose.production.yaml up --build -d
