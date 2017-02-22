a757f288c4ff1dfd106c6f33d1c421bcd236b680
Greg Walker <mgwalker@gmail.com>
2017-02-21 21:37:00 -0600

    First steps towards auto-deployment

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=/Users/mgwalker/.ssh/id_rsa darkcooger.net
