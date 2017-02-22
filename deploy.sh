a0cfeeb088b3e6328f9b3daa725f2782bbf2cab8
Greg Walker <mgwalker@gmail.com>
2017-02-21 23:22:33 -0600

    Almost there

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# Download docker-machine
-#curl -L https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-`uname -s`-`uname -m` > ./docker-machine
-#chmod +x ./docker-machine
-
-# Find our deployment key
-KEY=$(ls ~/.ssh | egrep '^id_[0-9a-f]{32}$')
-
 # Decrypt and unzip docker machine certs
-unzip docker-certs.zip -d certs
+unzip deploy
 
 # Add remote docker host
 #./docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=/home/ubuntu/.ssh/$KEY deploy-target
@@ -16,7 +9,7 @@ unzip docker-certs.zip -d certs
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://104.131.39.131:2376"
-export DOCKER_CERT_PATH="/home/ubuntu/certs"
+export DOCKER_CERT_PATH="/home/ubuntu/$CIRCLE_PROJECT_REPONAME/certs"
 export DOCKER_MACHINE_NAME="darkcooger.net"
 
 # Go!
