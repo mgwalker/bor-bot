f84dbeedfdf78d0ea76e2bf027dd4b9393316776
Greg Walker <mgwalker@gmail.com>
2017-02-21 21:53:37 -0600

    Maybe

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=/Users/mgwalker/.ssh/id_rsa darkcooger.net
+
+# Find our deployment key
+KEY=$(ls ~/.ssh | egrep '^id_[0-9a-f]{32}$')
+docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=~/.ssh/$KEY deploy-target
+eval $(docker-machine env deploy-target)
+docker-compose up --build
