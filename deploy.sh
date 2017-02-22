88f381e00e98d8bd2d866028a016e2b6e3388335
Greg Walker <mgwalker@gmail.com>
2017-02-21 22:26:49 -0600

    Install docker manually a different way, so we can get a recent enough version; modify deploy script to use absolute paths since docker-machine appears to be super dumb

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-`uname -s`-`uname -m` > ./docker-machine
+curl -L https://github.com/docker/machine/releases/download/v1.3.0/docker-machine-`uname -s`-`uname -m` > ./docker-machine
 chmod +x ./docker-machine
 
 # Find our deployment key
 KEY=$(ls ~/.ssh | egrep '^id_[0-9a-f]{32}$')
-./docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=~/.ssh/$KEY deploy-target
+./docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=/home/ubuntu/.ssh/$KEY deploy-target
 eval $(./docker-machine env deploy-target)
 docker-compose -f docker-compose.production.yaml up --build -d
