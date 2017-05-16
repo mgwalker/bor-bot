f2bf485617141fabe374a0adb753844d5f96b01f
Greg Walker <mgwalker@gmail.com>
2017-05-16 18:21:48 -0500

    Don't add a machine, that destroys and creates a new one - definitely not what we want

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@
 # Decrypt and unzip docker machine certs
 unzip deploy
 
-# Add remote docker host
-#./docker-machine create --driver generic --generic-ip-address=104.131.39.131 --generic-ssh-user=docker-deployer --generic-ssh-key=/home/ubuntu/.ssh/$KEY deploy-target
-#eval $(./docker-machine env deploy-target)
-
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://104.131.39.131:2376"
 export DOCKER_CERT_PATH="/home/ubuntu/$CIRCLE_PROJECT_REPONAME/certs"
